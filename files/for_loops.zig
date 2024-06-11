0a8078ca3940dacec6fb9630ce38cd838fca54a8
Nathan3-14 <nathan.watson.172@gmail.com>
2024-06-11 18:01:14 +0100

    Fun

---
+++ files/for_loops.zig
@@ -0,0 +1,16 @@
+test "for" {
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string, 0..) |character, index| {
+        _ = character;
+        _ = index;
+    }
+
+    for (string) |character| {
+        _ = character;
+    }
+
+    for (string, 0..) |_, index| {
+        _= index;
+    }
+}
